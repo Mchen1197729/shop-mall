59f910871eb5b3c4aaf299a71980c68fdec5637c
1197729488 <1197729488@qq.com>
2019-11-20 17:56:10 +0800

    commit update

--- src/components/chenMM/AllPickShopList.vue
+++ src/components/chenMM/AllPickShopList.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="all-pick-shop-list">
     <yd-list theme="4">
-      <yd-list-item v-for="(item, key) in list" :key="key">
+      <yd-list-item v-for="(item, key) in list" type="link" :href="item.href" :key="key">
         <img slot="img" :src="item.img">
-        <span slot="title">{{item.title}}</span>
+        <span class="shop-name" slot="title">{{item.title}}</span>
         <yd-list-other slot="other">
           <div>
-            <span class="demo-list-price"><em>¥</em>{{item.price}}</span>
-            <span class="demo-list-del-price">¥{{item.w_price}}</span>
+            <span class="open-time"><em>营业时间：</em>{{item.open_time}}</span>
           </div>
-          <div>content</div>
+          <div>
+            <img class="location" src="./images/location.png" alt="">
+          </div>
+        </yd-list-other>
+        <yd-list-other slot="other">
+          <div>
+            <span class="shop-address"><em>地址：</em>{{item.location}}</span>
+          </div>
+          <div class="shop-distance">{{item.distance}}</div>
         </yd-list-other>
       </yd-list-item>
     </yd-list>
@@ -27,57 +34,23 @@
         list: [
           {
             img: '//img1.shikee.com/try/2016/06/23/14381920926024616259.jpg',
-            title: '标题111标题标题标题标题',
+            title: '开源超市(80%返积分)',
             price: 156.23,
-            w_price: 89.36
+            w_price: 89.36,
+            open_time: '9:00-21:00',
+            location: '智慧大道1001号',
+            distance: '100米',
+            href:'/shopinfo?shop_id=1001&goods_id=9'
           },
           {
             img: '//img1.shikee.com/try/2016/06/21/10172020923917672923.jpg',
-            title: '标题222标题标题标题标题',
+            title: '开源超市(80%返积分)',
             price: 256.23,
-            w_price: 89.36
-          },
-          {
-            img: '//img1.shikee.com/try/2016/06/23/15395220917905380014.jpg',
-            title: '标题333标题标题标题标题',
-            price: 356.23,
-            w_price: 89.36
-          },
-          {
-            img: '//img1.shikee.com/try/2016/06/25/14244120933639105658.jpg',
-            title: '标题444标题标题标题标题',
-            price: 456.23,
-            w_price: 89.36
-          },
-          {
-            img: '//img1.shikee.com/try/2016/06/26/12365720933909085511.jpg',
-            title: '标题555标题标题标题标题',
-            price: 556.23,
-            w_price: 89.36
-          },
-          {
-            img: '//img1.shikee.com/try/2016/06/19/09430120929215230041.jpg',
-            title: '标题666标题标题标题标题',
-            price: 656.23,
-            w_price: 89.36
-          },
-          {
-            img: '//img1.shikee.com/try/2016/06/19/09430120929215230041.jpg',
-            title: '标题666标题标题标题标题',
-            price: 656.23,
-            w_price: 89.36
-          },
-          {
-            img: '//img1.shikee.com/try/2016/06/19/09430120929215230041.jpg',
-            title: '标题666标题标题标题标题',
-            price: 656.23,
-            w_price: 89.36
-          },
-          {
-            img: '//img1.shikee.com/try/2016/06/19/09430120929215230041.jpg',
-            title: '标题666标题标题标题标题',
-            price: 656.23,
-            w_price: 89.36
+            w_price: 89.36,
+            open_time: '9:00-21:00',
+            location: '智慧大道1001号',
+            distance: '100米',
+            href:'/shopinfo?shop_id=1001&goods_id=9'
           }
         ]
       }
@@ -89,6 +62,6 @@
   .all-pick-shop-list {
     width: 100%;
     height: 100%;
-    background-color: pink;
+    /*background-color: pink;*/
   }
 </style>
